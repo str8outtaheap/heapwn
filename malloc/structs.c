12b9e5b7637de4e438c04add74f34ad3acd8496b
xero <stratosuni@yahoo.gr>
2018-01-13 14:03:16 +0200

    Update structs.c

--- malloc/structs.c
+++ malloc/structs.c
@@ -1,3 +1,16 @@
+struct malloc_chunk {
+
+  INTERNAL_SIZE_T      mchunk_prev_size;  /* Size of previous chunk (if free).  */
+  INTERNAL_SIZE_T      mchunk_size;       /* Size in bytes, including overhead. */
+
+  struct malloc_chunk* fd;         /* double links -- used only if free. */
+  struct malloc_chunk* bk;
+
+  /* Only used for large blocks: pointer to next larger size.  */
+  struct malloc_chunk* fd_nextsize; /* double links -- used only if free. */
+  struct malloc_chunk* bk_nextsize;
+};
+
 struct malloc_state
 {
   /* Serialize access.  */
