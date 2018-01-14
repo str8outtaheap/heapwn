da7725df6b14e032335255a4eae230fc299b29fd
xero <stratosuni@yahoo.gr>
2018-01-15 01:13:16 +0200

    Update libioP.h

--- io/libioP.h
+++ io/libioP.h
@@ -1,37 +1,3 @@
-/* Copyright (C) 1993-2016 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.
-
-   As a special exception, if you link the code in this file with
-   files compiled with a GNU compiler to produce an executable,
-   that does not cause the resulting executable to be covered by
-   the GNU Lesser General Public License.  This exception does not
-   however invalidate any other reasons why the executable file
-   might be covered by the GNU Lesser General Public License.
-   This exception applies to code released by its copyright holders
-   in files containing the exception.  */
-
-/* NOTE: libio is now exclusively used only by glibc since libstdc++ has its
-   own implementation.  As a result, functions that were implemented for C++
-   (like *sputn) may no longer have C++ semantics.  This is of course only
-   relevant for internal callers of these functions since these functions are
-   not intended for external use otherwise.
-
-   FIXME: All of the C++ cruft eventually needs to go away.  */
-
 #include <stddef.h>
 
 #include <errno.h>
