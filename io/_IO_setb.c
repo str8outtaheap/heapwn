62ac7421e675a5dcf755eaf88cad21ea8f5c663a
xero <stratosuni@yahoo.gr>
2018-01-15 14:22:03 +0200

    Create _IO_setb.c

---
+++ io/_IO_setb.c
@@ -0,0 +1,13 @@
+void
+_IO_setb (_IO_FILE *f, char *b, char *eb, int a)
+{
+  if (f->_IO_buf_base && !(f->_flags & _IO_USER_BUF))
+    free (f->_IO_buf_base);
+  f->_IO_buf_base = b;
+  f->_IO_buf_end = eb;
+  if (a)
+    f->_flags &= ~_IO_USER_BUF;
+  else
+    f->_flags |= _IO_USER_BUF;
+}
+libc_hidden_def (_IO_setb)
