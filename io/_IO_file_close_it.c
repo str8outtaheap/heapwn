ad73b8c50dc4d3539303daabdc6ac6ec33e24658
xero <stratosuni@yahoo.gr>
2018-01-15 16:28:11 +0200

    Create _IO_file_close_it.c

---
+++ io/_IO_file_close_it.c
@@ -0,0 +1,41 @@
+int
+_IO_new_file_close_it (_IO_FILE *fp)
+{
+  int write_status;
+  if (!_IO_file_is_open (fp))
+    return EOF;
+
+  if ((fp->_flags & _IO_NO_WRITES) == 0
+      && (fp->_flags & _IO_CURRENTLY_PUTTING) != 0)
+    write_status = _IO_do_flush (fp);
+  else
+    write_status = 0;
+
+  _IO_unsave_markers (fp);
+
+  int close_status = ((fp->_flags2 & _IO_FLAGS2_NOCLOSE) == 0
+		      ? _IO_SYSCLOSE (fp) : 0);
+
+  /* Free buffer. */
+#if defined _LIBC || defined _GLIBCPP_USE_WCHAR_T
+  if (fp->_mode > 0)
+    {
+      if (_IO_have_wbackup (fp))
+	_IO_free_wbackup_area (fp);
+      _IO_wsetb (fp, NULL, NULL, 0);
+      _IO_wsetg (fp, NULL, NULL, NULL);
+      _IO_wsetp (fp, NULL, NULL);
+    }
+#endif
+  _IO_setb (fp, NULL, NULL, 0);
+  _IO_setg (fp, NULL, NULL, NULL);
+  _IO_setp (fp, NULL, NULL);
+
+  _IO_un_link ((struct _IO_FILE_plus *) fp);
+  fp->_flags = _IO_MAGIC|CLOSED_FILEBUF_FLAGS;
+  fp->_fileno = -1;
+  fp->_offset = _IO_pos_BAD;
+
+  return close_status ? close_status : write_status;
+}
+libc_hidden_ver (_IO_new_file_close_it, _IO_file_close_it)
